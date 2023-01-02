5dee48645d8cc2723f0088777c61d3abe8ee930c
PijusSerapinasMipsas <pijus.s@mipsas.com>
2023-01-02 19:02:19 +0200

    feat: pipelines

--- demo/generator/generator.go
+++ demo/generator/generator.go
@@ -1,4 +1,66 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateRandInt(min, max int) <-chan int {
+	out := make(chan int, 3)
+
+	go func() {
+		for {
+			out <- rand.Intn(max-min+1) + min
+		}
+	}()
+
+	return out
+}
+
+func generateRandIntn(count, min, max int) <-chan int {
+	out := make(chan int, 1)
+
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Intn(max-min+1) + min
+		}
+		close(out)
+	}()
+
+	return out
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	randInt := generateRandInt(1, 100)
+
+	fmt.Println("generatRandInt infinite")
+
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+
+	randIntnRange := generateRandIntn(2, 1, 100)
+
+	fmt.Println("generatRandInt using finite count")
+
+	for i := range randIntnRange {
+		fmt.Println(i)
+	}
+
+	fmt.Println("generatRandInt using infinite loop")
+
+	randIntn := generateRandIntn(5, 1, 100)
+	for {
+		n, open := <-randIntn
+		if !open {
+			break
+		}
+		fmt.Println(n)
+	}
 }
